fb905362ab9522bd0725f186cc2b629e9be0306b
Aayush <amaharjann01@gmail.com>
2023-02-06 13:35:09 -0800

    adding message.css file and adding padding

--- memory-card/src/components/Message/Message.jsx
+++ memory-card/src/components/Message/Message.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Message.css";
 
 export default function Message() {
     return (
-        <div>
+        <div className="message-container">
             Don't click the same picture twice.
         </div>
     )
